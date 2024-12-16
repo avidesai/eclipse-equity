38b3c330baf095972ded6277d343b13649aecabd
avi <avidesai2@gmail.com>
2024-12-15 22:14:39 -0800

    Move type definitions to dependencies

--- server/dist/middleware/auth.js
+++ server/dist/middleware/auth.js
@@ -6,9 +6,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token)
+    var _a;
+    const token = (_a = req.headers.authorization) === null || _a === void 0 ? void 0 : _a.split(' ')[1];
+    if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
+    }
     try {
         const decoded = jsonwebtoken_1.default.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
